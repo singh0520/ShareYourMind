a85be301d6d00f3f87bba8f94b78de99bdd2d24f
Nikhil Singh <singh0520@gmail.com>
2017-06-24 13:16:23 +0530

    mail js file for application

---
+++ copy.js
@@ -0,0 +1,22 @@
+
+var express = require('express');
+var app = express();
+var dataFile = require('./data/data.json');
+var reload = require('reload');
+
+app.set('port', process.env.PORT || 3000);
+app.set('appData', dataFile);
+app.set('view engine', 'ejs');
+app.set('views', 'app/views');
+
+app.locals.siteTitle = 'Roux Meetups';
+
+app.use(express.static('app/public'))
+app.use(require('./routes/index1.js'));
+app.use(require('./routes/speakers1.js'));
+
+var server = app.listen(app.get('port'), function(){
+	console.log('Listening on my port ' + app.get('port'));
+});
+
+reload(server, app);
